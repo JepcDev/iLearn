38ee0412f623bc58c41e00d67997b7679d858519
JepcDev <joseenrique.pc21@gmail.com>
2024-10-05 06:55:11 -0400

    ADD: first commit

---
+++ movil/dart/tipos_Datos.dart
@@ -0,0 +1,22 @@
+void main(List<String> arguments) {
+  print('Hello world c526');
+  // Tipos de variables
+  // tipo + nombre_variable = valor;
+
+  // Entero
+  /* int? entero;
+  print(entero); */
+
+  // Double
+  /* double real;
+  real = 3.1416;
+  print(real); */
+
+  //Texto
+  /* String texto = 'Hello world 5010 $real\n';
+  print(texto); */
+
+  // Boolean
+  bool boleano = true;
+  print(boleano);
+}
