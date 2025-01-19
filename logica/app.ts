afcce80071c408dc1fe4df3ac97454f57e4f539c
JepcDev <joseenrique.pc21@gmail.com>
2025-01-19 00:59:03 -0400

    feat: add functions aplications, definition and examples

--- logica/app.ts
+++ logica/app.ts
@@ -3,9 +3,23 @@
 // Una fucnion es un conjunto de lineas de codigo que realizan una tarea especifica y pueden retornar algun resultado.
 // un parametro o argumento: es algun valor(datos) que le enviamos a la funcion para que pueda realizar alguna tarea.
 
-function sumar(a:number , b:number) { //parametos o argumentos: cuando hay mas de un argumento se separan por coma.
-  return a + b;
+// function sumar(a:number , b:number) { //parametos o argumentos: cuando hay mas de un argumento se separan por coma.
+//   return a + b;
+// }
+
+// let total = sumar(10,20);
+// console.log("Total = ",total);
+
+export function greet(name: string = 'Mundo') {
+  console.log('Hola ' + name);
 }
 
-let total = sumar(10,20);
-console.log("Total = ",total);
+// let firtName = 'Jose';
+// greet(firtName);
+
+function addTwoNumbers( num1:number, num2:number = 0): number {
+  return num1 + num2;
+}
+
+let total: number = addTwoNumbers(10 , 20);
+console.log('Total: ', total);
