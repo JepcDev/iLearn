a1493610b4dcec40d049f7b237c19352bd769699
JepcDev <joseenrique.pc21@gmail.com>
2025-01-21 21:44:45 -0400

    feat: add arrays homework

--- logica/app.ts
+++ logica/app.ts
@@ -2,9 +2,16 @@
 
 // Es un conjunto de cosas similares, del mismo tipo de dato.
 
-let people: string[]=['Fernando', 'Melisa', 'Eduardo'];
-let flowers: string[] = ['Rosa', 'Girasol', 'Lirio'];
+let employees: string[]=['Fernando', 'Melisa', 'Eduardo'];
+let salaries: number[] = [1500,2400,3200];
 
-for (let index = 0; index < flowers.length; index++) {
-  console.log(flowers[index]);
+// let flowers: string[] = ['Rosa', 'Girasol', 'Lirio'];
+
+// for (let index = 0; index < flowers.length; index++) {
+//   console.log(flowers[index]);
+// }
+
+// DEV:TODO -> Task: imprimir los empleados con sus respectivos salarios, "Fernando tiene un salario de 1500"
+for (let index = 0; index < employees.length; index++) {
+  console.log(employees[index],'tiene un salario de',salaries[index]);
 }
