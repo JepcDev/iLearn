53964c9dccab3852108c0a8fcfe3ce6fd0f94a94
JepcDev <joseenrique.pc21@gmail.com>
2025-01-21 21:19:36 -0400

    feat: add arrays and examples

--- logica/app.ts
+++ logica/app.ts
@@ -1,1 +1,10 @@
-console.log('Hi mundo');
+// ANCHOR -> Arreglos
+
+// Es un conjunto de cosas similares, del mismo tipo de dato.
+
+let people: string[]=['Fernando', 'Melisa', 'Eduardo'];
+let flowers: string[] = ['Rosa', 'Girasol', 'Lirio'];
+
+for (let index = 0; index < flowers.length; index++) {
+  console.log(flowers[index]);
+}
